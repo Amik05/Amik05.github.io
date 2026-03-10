2bc1fef9d69d187e50c8d912496bff034cacec84
Amik05 <amatianiu@gmail.com>
2026-03-09 18:41:15 -0700

    fix: put all images in the public folder

--- src/components/contents/OtherContent.jsx
+++ src/components/contents/OtherContent.jsx
@@ -1,7 +1,7 @@
 const OtherContent = () => {
   return (
     <div className="flex items-center mx-10 gap-10 border-b-2 border-black/50 dark:border-white/80">
-      <img src="../src/assets/rotatingCar.gif" alt="car" className="w-50" />
+      <img src="/rotatingCar.gif" alt="car" className="w-50" />
       <div className="mr-10 text-black/80 dark:text-white flex flex-col gap-2">
         <h1 className="text-3xl my-2">vrooom</h1>
         <p>"I am speed"</p>
--- src/components/contents/ProjectsContent.jsx
+++ src/components/contents/ProjectsContent.jsx
@@ -1,7 +1,7 @@
 const ProjectsContent = () => {
   return (
     <div className="flex items-center mx-10 gap-10 border-b-2 border-black/50 dark:border-white/80">
-      <img src="../src/assets/orange.png" alt="orange" className="w-50" />
+      <img src="/orange.png" alt="orange" className="w-50" />
       <div className="mr-10 text-black/80 dark:text-white flex flex-col gap-2">
         <h1 className="text-3xl my-2">projects</h1>
       </div>
